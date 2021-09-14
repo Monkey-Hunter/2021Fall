51529c9e0ad6964c37c853d995de04ee72ef603a
Monkey-Hunter <kelvin4379@gmail.com>
2021-09-14 20:38:03 +0900

    turtleLeg boj1038

---
+++ WEEK2/turtleLeg/boj1038/boj1038.java
@@ -0,0 +1,27 @@
+package turtleLeg.boj1038;
+
+import java.io.*;
+import java.util.*;
+
+public class boj1038 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        int q_maxsize = 1;
+        int[] arr = {0,1,2,3,4,5,6,7,8,9};
+
+        if(N>1022)  
+            System.out.print(-1);
+
+
+
+        while(N>0){
+
+        }
+
+    }
+}
