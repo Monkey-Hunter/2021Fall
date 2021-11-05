31c302a255bdff92bd6c2e85271fafeac1201fc8
Monkey-Hunter <kelvin4379@gmail.com>
2021-11-05 11:12:38 +0900

    turtleLeg

--- WEEK3/turtleLeg/boj9019/Main.java
+++ WEEK3/turtleLeg/boj9019/Main.java
@@ -5,72 +5,84 @@ import java.util.*;
 
 public class Main {
 
-    public static StringBuilder sb = new StringBuilder();
+	static class State {
+		int now;
+		String ans;
 
+		public State(int now, String ans) {
+			super();
+			this.now = now;
+			this.ans = ans;
+		}
 
-    public static int D(int num){
-        int res = num*2;
-        if(res > 9999)
-            res %= 10000;
-        
-        return res;
-    }
+	}
+	static int A,B;
+	static boolean[] chk;
 
-    public static int S(int num){
-        int res;
-        if(num == 0) res = 9999;
-        else res = num - 1;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			chk = new boolean[10000];
+			chk[A] = true;
+			
+			Queue<State> q = new LinkedList<State>();
+			q.offer(new State(A, ""));
+			while (!q.isEmpty()) {
+				State s = q.poll();
+				
+				if (s.now == B) {
+					System.out.println(s.ans);
+					break;
+				}
+				int D = 2 * s.now;
+				int S = s.now - 1;
+				// D
+				if (D > 9999) {
+					D%=10000;
+					if (chk[D] == false) {
+						chk[D] = true;
+						q.offer(new State(D, s.ans+"D"));
+					}
+				} else {
+					if (chk[D] == false) {
+						chk[D] = true;
+						q.offer(new State(D, s.ans+"D"));
+					}
+				}
+				// S
+				if (S == -1) {
+					if (chk[9999] == false) {
+						chk[9999] = true;
+						q.offer(new State(9999,  s.ans+"S"));
+					}
+				} else {
+					if (chk[S] == false) {
+						chk[S] = true;
+						q.offer(new State(S,  s.ans+"S"));
 
-        return res;
-    }
+					}
+				}
+				// L
+				int tmp=(s.now%1000)*10;
+				tmp+=s.now/1000;
+				if (chk[tmp] == false) {
+					chk[tmp] = true;
+					q.offer(new State(tmp,s.ans+"L"));
+				}
+				// R
+				tmp=(s.now%10)*1000;
+				tmp+=(s.now/10);
+				if (chk[tmp] == false) {
+					chk[tmp] = true;
+					q.offer(new State(tmp,s.ans+"R"));
+				}
+			}
+		}
+	}
 
-    public static int L(int num){
-        int d4 = num/1000;
-        num -= d4*1000;
-        int d3 = num/100;
-        num -= d3*100;
-        int d2 = num/10;
-        num -= d2*10;
-        int d1 = num;
-
-        int res = d2*1000 + d3*100 + d4*10 + d1;
-
-        return res;
-    }
-
-    public static int R(int num){
-        int d4 = num/1000;
-        num -= d4*1000;
-        int d3 = num/100;
-        num -= d3*100;
-        int d2 = num/10;
-        num -= d2*10;
-        int d1 = num;
-
-        int res = d4*1000 + d1*100 + d2*10 + d3;
-
-        return res;
-    }
-
-    public static void solve(int num, int taret){
-        
-    }
-
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int N = Integer.parseInt(br.readLine());
-
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            int target = Integer.parseInt(st.nextToken());
-            solve(num, target);
-            sb.append("\n");
-        }
-
-        System.out.print(sb);
-    }
 }
